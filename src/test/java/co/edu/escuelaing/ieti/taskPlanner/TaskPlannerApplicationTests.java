a9ef3224b580784070f6a3990e027a4216698180
juanmd9 <juanmmd9@gmail.com>
2021-03-05 08:12:26 -0500

    first commit

---
+++ src/test/java/co/edu/escuelaing/ieti/taskPlanner/TaskPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.ieti.taskPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
