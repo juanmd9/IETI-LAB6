098cbe362dc7d796047170173f7a833acc056d60
juanmd9 <juanmmd9@gmail.com>
2021-03-07 12:55:19 -0500

    Cors autorizados

--- src/main/java/co/edu/escuelaing/ieti/taskPlanner/TaskPlannerApplication.java
+++ src/main/java/co/edu/escuelaing/ieti/taskPlanner/TaskPlannerApplication.java
@@ -1,13 +1,28 @@
 package co.edu.escuelaing.ieti.taskPlanner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TaskPlannerApplication {
+@PropertySource("classpath:application.properties")
+public class TaskPlannerApplication implements WebMvcConfigurer {
+
+	@Value( "${cors.urlsValidos}" )
+	String urlsValidos;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskPlannerApplication.class, args);
 	}
 
+	public void addCorsMappings(CorsRegistry registry) {
+
+		String[] origins = urlsValidos.split("#");
+		registry.addMapping("/**")
+				.allowedOrigins(origins)
+				.allowedMethods("POST", "GET");
+	}
 }
--- src/main/java/co/edu/escuelaing/ieti/taskPlanner/service/impl/UserServiceImpl.java
+++ src/main/java/co/edu/escuelaing/ieti/taskPlanner/service/impl/UserServiceImpl.java
@@ -11,16 +11,25 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private User user = User.builder()
+            .id(Long.parseLong("1"))
             .name("Juan")
             .lastName("Munoz")
             .email("juan@mail.com")
             .password("123")
+            .build();
+    private User user1 = User.builder()
+            .id(Long.parseLong("2"))
+            .name("Daniel")
+            .lastName("Fernandez")
+            .email("daniel@mail.com")
+            .password("0987")
             .build();
 
     @Override
     public List<User> getAll() {
         List<User> users = new ArrayList<>();
         users.add(user);
+        users.add(user1);
         return users;
     }
 
